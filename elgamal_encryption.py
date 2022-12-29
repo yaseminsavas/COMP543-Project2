c4c15c43e35745f2bc02ac7bd9e67f3c2e6b9813
Yasemin Savas <37246409+yaseminsavas@users.noreply.github.com>
2022-12-29 16:11:39 +0300

    fix

--- elgamal_encryption.py
+++ elgamal_encryption.py
@@ -64,10 +64,9 @@ def elgamal_key_generator():
     h = pow(g, b)
     public_key = (F_q, h, q, g)
 
-    # Retaining private key
-    private_key_1 = b
-    with open('private_key.txt', 'w') as f:
-        f.write(str(private_key_1))
+    # Retaining private key (Bob)
+    with open('private_key_bob.txt', 'w') as f:
+        f.write(str(b))
         f.close()
 
     return public_key
@@ -92,6 +91,11 @@ def elgamal_encryption(public_key, plaintext):
         if math.gcd(k, g) == 1:
             break
 
+    # Retaining private key (Alice)
+    with open('private_key_alice.txt', 'w') as f:
+        f.write(str(k))
+        f.close()
+
     p = pow(g, k)
     s = pow(h, k)
 
@@ -113,7 +117,7 @@ b. Bob decrypts M x s with s’ and obtains M.
 
 def elgamal_decryption(p, ciphertext):
 
-    with open('private_key.txt', 'r') as f:
+    with open('private_key_bob.txt', 'r') as f:
         private_key = int(f.readlines()[0])
         f.close()
 
@@ -131,13 +135,16 @@ def write_to_server(public_key, ciphertext, p):
 
     cipher = ""
     for i in range(0, msg_length):
-        cipher += str(ciphertext[i]) + ","
+        if i < msg_length - 1:
+            cipher += str(ciphertext[i]) + ","
+        else:
+            cipher += str(ciphertext[i])
 
     F = str("F:" + str(public_key[0]))
     H = str("H:" + str(public_key[1]))
     Q = str("Q:" + str(public_key[2]))
     G = str("G:" + str(public_key[3]))
-    C = str("C:" + ascii(str(cipher)))
+    C = str("C:" + str(cipher))
     P = str("p:" + str(p))
 
     f = open("server.txt", "w")
@@ -153,18 +160,32 @@ def write_to_server(public_key, ciphertext, p):
 chat_count = 0
 while True:
 
-    if os.path.exists("server.txt") is False:
+    # initializing the chat server, sending the first msg
+    if os.path.exists("server.txt") is False and chat_count == 0:
+        print("COMMUNICATION STARTED, GENERATING THE SERVER...")
+
+        public_key = elgamal_key_generator()
         with open('server.txt', 'x') as f:
             f.close()
 
-    print("CHECKING THE SERVER...")
-    sleep(5)
+        plaintext = input("SEND A MESSAGE: ")
+        p, ciphertext = elgamal_encryption(public_key, plaintext)
 
-    if os.path.getsize('server.txt') == 0 or chat_count == 0:
+        ctext = ""
+        for i in ciphertext:
+            ctext += str(i)
+        write_to_server(public_key, ciphertext, p)
+        chat_count += 1
+        print("MESSAGE SENT...")
+
+        print("CHECKING THE SERVER FOR AN INCOMING MESSAGE...")
+        sleep(5)
+
+    # sending the first msg in an existing server
+    elif os.path.exists("server.txt") is True and os.path.getsize('server.txt') == 0 and chat_count == 0:
 
         print("NO INCOMING MESSAGES...")
         plaintext = input("SEND A MESSAGE: ")
-
         public_key = elgamal_key_generator()
         p, ciphertext = elgamal_encryption(public_key, plaintext)
 
@@ -173,34 +194,63 @@ while True:
             ctext += str(i)
         write_to_server(public_key, ciphertext, p)
         chat_count += 1
+        print("MESSAGE SENT...")
+
+        print("CHECKING THE SERVER FOR AN INCOMING MESSAGE...")
+        sleep(5)
+
+    # communication in later steps
+    elif os.path.exists("server.txt") is True and os.path.getsize('server.txt') > 0:
+
+        with open('server.txt', 'r') as f:
+            lines = f.readlines()
+
+        F, H, Q, G, ciphertext, p = lines[0][2:], lines[1][2:], lines[2][2:], lines[3][2:], lines[4][2:], lines[5][2:]
+        ciphertext = ciphertext.split(",")
+        plaintext = elgamal_decryption(int(p), ciphertext)
+
+        str_ptext = ""
+        for i in plaintext:
+            str_ptext += i
+        print("INCOMING MESSAGE:", ascii(str_ptext))
+
+        # deleting the server contents after decrypting an incoming message.
+        open('server.txt', 'w').close()
+
+        plaintext = input("ANSWER THE MESSAGE: ")
+
+        F_updated = F[1:-2].split(", ")
+        F_int = []
+        for i in F_updated:
+            F_int.append(int(i))
+
+        public_key = (F_int, int(H), int(Q), int(G))
+        p, ciphertext = elgamal_encryption(public_key, plaintext)
+
+        ctext = ""
+        for i in ciphertext:
+            ctext += str(i)
+        write_to_server(public_key, ciphertext, p)
+
+        print("CHECKING THE SERVER FOR AN INCOMING MESSAGE...")
+        sleep(5)
+
+    elif os.path.exists("server.txt") is True and os.path.getsize('server.txt') == 0 and chat_count > 0:
+
+        print("NO INCOMING MESSAGES...")
+        plaintext = input("SEND A MESSAGE: ")
+        p, ciphertext = elgamal_encryption(public_key, plaintext)
+
+        ctext = ""
+        for i in ciphertext:
+            ctext += str(i)
+        write_to_server(public_key, ciphertext, p)
+        chat_count += 1
+        print("MESSAGE SENT...")
+
+        print("CHECKING THE SERVER FOR AN INCOMING MESSAGE...")
+        sleep(5)
 
     else:
-        print("CHECKING THE SERVER...")
-        sleep(5)
-        try:
-            with open('server.txt', 'r') as f:
-                lines = f.readlines()
-
-            F, H, Q, G, ciphertext, p = lines[0][2:], lines[1][2:], lines[2][2:], lines[3][2:], lines[4][2:], lines[5][2:]
-
-            ciphertext = ciphertext.split(",")[:-1]
-            plaintext = elgamal_decryption(int(p), ciphertext)
-
-            str_ptext = ""
-            for i in plaintext:
-                str_ptext += i
-            print("INCOMING MESSAGE:", ascii(str_ptext))
-            open('server.txt', 'w').close()
-
-            plaintext = input("ANSWER THE MESSAGE: ")
-            p, ciphertext = elgamal_encryption(public_key, plaintext)
-
-            ctext = ""
-            for i in ciphertext:
-                ctext += str(i)
-
-            write_to_server(public_key, ciphertext, p)
-
-        except:
-            print("WAITING FOR AN INCOMING MESSAGE...")
-            sleep(5)
+        print("AN ERROR OCCURRED. DELETE THE SERVER FILE & RESTART")
+        break
